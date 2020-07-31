03d2a29b4a86628465a7cfe13eade9a55282b7cc
mkkulchourasia24 <mukulch24@gmail.com>
2020-07-31 12:29:46 +0530

    prod.exs updated and user_socket.ex updated

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ config :discuss, DiscussWeb.Endpoint,
   load_from_system_env: true,
   url: [scheme: "https", host: "cryptic-citadel-89398.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: :x_forwarded_proto],
-  cache_static_manifest: "priv/static/cache_manifest.json",
+#  cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 config :discuss, Discuss.Repo,
